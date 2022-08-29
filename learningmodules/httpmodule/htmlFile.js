db0fbc4d1262b9cfe006b7b8b6835184fbf191a2
aaaasssshhhhiiiisssshhhh <aashishkarki11@gmail.com>
2022-08-29 22:17:56 +0545

    serving html efficently

--- learningmodules/httpmodule/htmlFile.js
+++ learningmodules/httpmodule/htmlFile.js
@@ -4,23 +4,61 @@ const fs = require("fs").promises;
 const host = "localhost";
 const port = 8000;
 
+//Serving an HTML Page From a File
+// const requestListener = function (req, res) {
+//   fs.readFile(__dirname + "/index.html")
+//     .then((contents) => {
+//       res.setHeader("Content-Type", "Text/html");
+//       res.writeHead(200);
+//       res.end(contents);
+//       console.log("request received");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.writeHead(500);
+//       res.end(`<h1>page you requested was not found</h1>`);
+//       return;
+//     });
+// };
+
+// const server = http.createServer(requestListener);
+// server.listen(port, host, () => {
+//   console.log(`server is running on http://${host}:${port}`);
+// });
+
+// serving html efficently
+
 const requestListener = function (req, res) {
-  fs.readFile(__dirname + "/index.html")
-    .then((contents) => {
-      res.setHeader("Content-Type", "Text/html");
-      res.writeHead(200);
-      res.end(contents);
-      console.log("request received");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.writeHead(500);
-      res.end(`<h1>page you requested was not found</h1>`);
-      return;
-    });
+  res.setHeader("Content-Typye", "text/html");
+  res.writeHead(200);
+  res.end(indexFile);
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`server is running on http://${host}:${port}`);
-});
+
+fs.readFile(__dirname + "/index.html")
+  .then((contents) => {
+    indexFile = contents;
+    server.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Could not read index.html file: ${err}`);
+    process.exit(1);
+  });
+
+// const server = http.createServer(requestListener);
+
+// fs.readFile(__dirname + "/index.html")
+//   .then((contents) => {
+//     indexFile = contents;
+//     server.listen(port, host, () => {
+//       console.log(`server is running on http://${host}:${port}`);
+//     });
+//   })
+
+//   .catch((err) => {
+//     console.error(`could not read the index.html file: ${err}`);
+//     process.exit(1);
+//   });
